68b5623b45a763b29cdd63a0e7e21179aa031c0a
monicamondo <monica.mondo@jexperts.com.br>
2026-04-01 12:24:07 -0300

    Protótipo conceito KRs

--- pdf_parser/reader.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const pdfParser = require('pdf-parse');
-
-const dataBuffer = fs.readFileSync('C:\\Dev\\Jexperts\\Channel\\prototypes-channel\\R1-RQ4\\docs\\RQ004+-+Criar+o+conceito+de+resultados+chave.pdf');
-pdfParser(dataBuffer).then(function(data) {
-    console.log(data.text);
-});
